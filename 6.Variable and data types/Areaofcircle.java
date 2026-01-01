5930a49cdc7b4cbc0d90dec191896c710de7840f
Swapnil Pawar <swapnildpawar011@gmail.com>
2026-01-01 19:49:02 +0530

    area of circle and type of conversion of datatypes in java

---
+++ 6.Variable and data types/Areaofcircle.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Areaofcircle{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        float a=3.14f;
+        int radius=sc.nextInt();
+        float Area=a*radius*radius;
+        System.out.println(Area);
+    }
+}
