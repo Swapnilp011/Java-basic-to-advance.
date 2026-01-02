8730b5e8dec089385e9c2715853dd6e3ea00f1a2
Swapnil Pawar <swapnildpawar011@gmail.com>
2026-01-02 08:29:13 +0530

    Perform Basic Logical Operators to campair the two statement value (statement)

---
+++ 7.Operators/LogicalOperators.java
@@ -0,0 +1,11 @@
+public class LogicalOperators{
+    public static void main(String args[]){
+        System.out.println((12>10)&&(10<12));
+        System.out.println((12<10)&&(10<12));
+        System.out.println((12>10)||(10<12));
+        System.out.println((12<10)||(10<12));
+        System.out.println(!(12>10));
+        System.out.println(!(12<10));
+
+    }
+}
