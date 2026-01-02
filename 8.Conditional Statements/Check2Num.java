e89a1f8dd0c937f4ee26e9a5160e74e367972e50
Swapnil Pawar <swapnildpawar011@gmail.com>
2026-01-02 19:11:23 +0530

    Check 2 Number using if else

---
+++ 8.Conditional Statements/Check2Num.java
@@ -0,0 +1,12 @@
+public class Check2Num{
+    public static void main(String args[]){
+        int a=1;
+        int b=3;
+        if(a<=b){
+            System.out.println("a is small");
+        }
+        else{
+            System.out.println("B is small");
+        }
+    }
+}
