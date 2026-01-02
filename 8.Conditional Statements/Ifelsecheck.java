95a7d2590a3a2215719187f84bef76b8cc4fa7aa
Swapnil Pawar <swapnildpawar011@gmail.com>
2026-01-02 18:59:12 +0530

    Simple If else check statement check adult or not

---
+++ 8.Conditional Statements/Ifelsecheck.java
@@ -0,0 +1,13 @@
+public class Ifelsecheck{
+    public static void main(String args[]){
+        int age =22;
+        if(age>=18){
+            System.out.println(
+                "You are adult you can vote"
+            );
+        }
+        else{
+            System.out.println("You are not adult");
+        }
+    }
+}
