56b647e5b3f8d442228d30a4a0b7f5792b523fe8
Swapnil Pawar <swapnildpawar011@gmail.com>
2026-01-03 08:33:45 +0530

    Perform operation of using Ternery Operator check even or odd number

---
+++ 8.Conditional Statements/TerneryOperator.java
@@ -0,0 +1,7 @@
+public class TerneryOperator{
+    public static void main(String args[]){
+        int num =4;
+        String type=((num%2)==0)? "It is Even" : "It is odd";
+        System.out.println(type);
+    }
+}
