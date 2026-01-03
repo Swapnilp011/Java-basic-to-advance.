0678c96795587ce80c2b26fe1eceb7c2889630f3
Swapnil Pawar <swapnildpawar011@gmail.com>
2026-01-03 18:27:09 +0530

    Print the number from 1 to 10 using the while loop

---
+++ 9.Loops (Flow Control)/Print1to10usewhileloop.java
@@ -0,0 +1,10 @@
+public class Print1to10usewhileloop{
+    public static void main(String args[]){   
+    int number=1;
+    while(number<=10){
+        System.out.println(number);
+        number++;
+    }
+    System.out.println("1 to 10 is printed");
+}
+}
