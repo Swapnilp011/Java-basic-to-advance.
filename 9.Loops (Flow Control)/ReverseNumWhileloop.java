6312851c30cb9a33ac9e67ab5d55ae3b39c0b9bb
Swapnil Pawar <swapnildpawar011@gmail.com>
2026-01-03 19:53:18 +0530

    Print the reverse number using the while loop

---
+++ 9.Loops (Flow Control)/ReverseNumWhileloop.java
@@ -0,0 +1,11 @@
+public class ReverseNumWhileloop{
+    public static void main(String args[]){
+        int num=12345;
+        while(num>0){
+            int lastdigit=num%10;
+            System.out.print(lastdigit +"");
+
+            num=num/10;
+        }
+    }
+}
