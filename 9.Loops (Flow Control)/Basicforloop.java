74dfacbbd4bddcd9d109f0657a4773e640a20024
Swapnil Pawar <swapnildpawar011@gmail.com>
2026-01-03 19:19:34 +0530

    Print the Hello world for 5 time using the for loop basic for loop code

---
+++ 9.Loops (Flow Control)/Basicforloop.java
@@ -0,0 +1,8 @@
+public class Basicforloop{
+    public static void main(String args[]){
+        // int i=1;
+        for(int i=1; i<=5; i++){
+            System.out.println("Hello World");
+        }
+    }
+}
