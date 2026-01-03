4a829f9c6daf1786171889a2895a5e8588848f53
Swapnil Pawar <swapnildpawar011@gmail.com>
2026-01-03 18:20:13 +0530

    Print Hello world for 100 times using while loop

---
+++ 9.Loops (Flow Control)/PrintHellowhileloop.java
@@ -0,0 +1,10 @@
+public class PrintHellowhileloop{
+    public static void main(String args[]){
+        int countHello=0;
+        while (countHello<100){
+            System.out.println("Hello World");
+            countHello++;
+        }
+        System.out.println("Hello World printed 100 times ");
+    }
+}
