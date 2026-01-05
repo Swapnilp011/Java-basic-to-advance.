c92916eca2566cbddd3d4c584f40cdb8de256204
Swapnil Pawar <swapnildpawar011@gmail.com>
2026-01-05 08:07:16 +0530

    Print user input  number up to when the user enter the multiple of 10

---
+++ 9.Loops (Flow Control)/Dowhilemulof10.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Dowhilemulof10{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        do{
+            int num=sc.nextInt();
+            if(num%10==0){
+                break;
+            }
+            System.out.println(num);
+        }while (true);
+    }
+}
