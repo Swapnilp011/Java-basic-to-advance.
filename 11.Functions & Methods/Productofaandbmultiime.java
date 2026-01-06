9b86071c44bf3008c51e828245c532e638fc060d
Swapnil Pawar <swapnildpawar011@gmail.com>
2026-01-06 19:14:56 +0530

    Find the produnt of a and b using the funtion and then call it but the print statement is witten in main funtion but in this Program call the funtion 2 time

---
+++ 11.Functions & Methods/Productofaandbmultiime.java
@@ -0,0 +1,17 @@
+public class Productofaandbmultiime{
+    public static int multiply(int a, int b){
+        int mul=a*b;
+        return mul;
+    }
+    public static void main(String args[]){
+        int a=15;
+        int b=2;
+        int prod=multiply(a, b);
+        System.out.println("The Product of a and b is: " + prod);
+        prod=multiply(40, 5);
+        System.out.println("The Product of a and b is: " + prod);
+
+
+
+    }
+}
