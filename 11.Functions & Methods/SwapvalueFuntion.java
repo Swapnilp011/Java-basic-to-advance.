5eeac837c4f4bbf732acc3179888048c7263bde3
Swapnil Pawar <swapnildpawar011@gmail.com>
2026-01-06 18:42:40 +0530

    Swaping the value of a and b using the funtion

---
+++ 11.Functions & Methods/SwapvalueFuntion.java
@@ -0,0 +1,13 @@
+public class SwapvalueFuntion{
+    public static void main(String args[]){
+        int a=10;
+        int b=20;
+
+        //Swap Funtion
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println("A value is : "+a);
+        System.out.println("B value is : "+b);
+    }
+}
