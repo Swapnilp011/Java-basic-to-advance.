b19c2b5609f0e626a97534c000b4b13c99eeb103
Swapnil Pawar <swapnildpawar011@gmail.com>
2026-01-06 19:10:35 +0530

    Find the produnt of a and b using the  funtion and then call it but the print statement is witten in main funtion

---
+++ 11.Functions & Methods/Productofaandb.java
@@ -0,0 +1,13 @@
+public class Productofaandb{
+    public static int multiply(int a, int b){
+        int mul=a*b;
+        return mul;
+    }
+    public static void main(String args[]){
+        int a=15;
+        int b=2;
+        int prod=multiply(a, b);
+        System.out.println("The Product of a and b is: " + prod);
+
+    }
+}
